199101e2c1ddce56b180a3f9a38b7150d741345b
Julia Leal <julialeal.t@gmail.com>
2024-06-06 10:29:21 -0300

    first commit

---
+++ tests/login.test.ts
@@ -0,0 +1,13 @@
+import LoginPage from "../screens/login.page"
+
+describe ('Login', function () {
+    
+    it('Teste', async function() {
+
+        await driver.pause(5000);
+
+
+        //await LoginPage.login('', '')
+    })
+
+})
